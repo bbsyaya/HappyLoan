6fb216ac578e30a11289f6a8a424550ec353dd39
fly-ouyang <fly_ouyang@qq.com>
2016-04-19 11:12:43 +0800

    Initial commit

---
+++ 快乐贷/src/main/java/fly/com/happyloan/Fragment/LoanFragment.java
@@ -0,0 +1,44 @@
+package fly.com.happyloan.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import fly.com.happyloan.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoanFragment extends Fragment {
+
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter adapter;
+    public LoanFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_loan, container, false);
+        init();
+        ListView list_loan = (ListView) view.findViewById(R.id.list_loan);
+        adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,list);
+        list_loan.setAdapter(adapter);
+        return view;
+    }
+
+    private void init(){
+        list.clear();
+        for (int i = 0 ; i < 10 ; i++){
+            list.add("借贷"+i);
+        }
+    }
+}
---
+++ 快乐贷/src/main/java/fly/com/happyloan/MainActivity.java
@@ -0,0 +1,80 @@
+package fly.com.happyloan;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import fly.com.happyloan.Fragment.FindsFragment;
+import fly.com.happyloan.Fragment.FriendsFragment;
+import fly.com.happyloan.Fragment.LoanFragment;
+import fly.com.happyloan.Fragment.MeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frame_content;
+    TextView tv_loan;
+    TextView tv_friends;
+    TextView tv_finds;
+    TextView tv_me;
+
+    FragmentManager fm = null;
+    FragmentTransaction ft = null; //Fragment事务
+    LoanFragment loan_fragment;
+    FriendsFragment friends_fragment;
+    FindsFragment finds_fragment;
+    MeFragment me_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById();
+        Fragment();
+
+    }
+    private void findViewById(){
+        frame_content = (FrameLayout) findViewById(R.id.frame_content);
+
+        tv_loan = (TextView) findViewById(R.id.tv_loan);
+        tv_friends = (TextView) findViewById(R.id.tv_friends);
+        tv_finds = (TextView) findViewById(R.id.tv_finds);
+        tv_me = (TextView) findViewById(R.id.tv_me);
+    }
+    private void Fragment(){
+        fm = getSupportFragmentManager();
+
+        loan_fragment = new LoanFragment();
+        friends_fragment = new FriendsFragment();
+        finds_fragment = new FindsFragment();
+        me_fragment = new MeFragment();
+
+        //默认
+        ft = fm.beginTransaction();
+        ft.add(R.id.frame_content,loan_fragment);
+        ft.commit();
+    }
+    public void change(View v){
+        if (v.getId() == R.id.tv_loan){
+            ft = fm.beginTransaction();
+            ft.replace(R.id.frame_content, loan_fragment);
+            ft.commit();
+        }else if (v.getId() == R.id.tv_friends){
+            ft = fm.beginTransaction();
+            ft.replace(R.id.frame_content, friends_fragment);
+            ft.commit();
+        }else if (v.getId() ==R.id.tv_finds){
+            ft = fm.beginTransaction();
+            ft.replace(R.id.frame_content, finds_fragment);
+            ft.commit();
+        }else if (v.getId() == R.id.tv_me) {
+            ft = fm.beginTransaction();
+            ft.replace(R.id.frame_content, me_fragment);
+            ft.commit();
+        }
+    }
+}
